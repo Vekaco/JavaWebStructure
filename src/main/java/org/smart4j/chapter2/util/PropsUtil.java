f4fa326af5d31515b2eb43912132a5a5868c94e6
Vekaco <dr.vekaco@gmail.com>
2018-06-27 21:27:05 +0800

    java web structure

---
+++ src/main/java/org/smart4j/chapter2/util/PropsUtil.java
@@ -0,0 +1,9 @@
+package org.smart4j.chapter2.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PropsUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+
+}
